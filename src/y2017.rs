561fd255bbe58e09f2610572654c087ac868ebd2
jsgroth <1137683+jsgroth@users.noreply.github.com>
2022-12-30 22:12:40 -0600

    2017 day 4

--- src/y2017.rs
+++ src/y2017.rs
@@ -1,6 +1,7 @@
 mod day1;
 mod day2;
 mod day3;
+mod day4;
 
 use std::error::Error;
 use crate::SimpleError;
@@ -10,6 +11,7 @@ pub fn run_day(day: usize) -> Result<(), Box<dyn Error>> {
         1 => crate::run_solution(day1::solve),
         2 => crate::run_solution(day2::solve),
         3 => crate::run_solution(day3::solve),
+        4 => crate::run_solution(day4::solve),
         _ => Err(Box::new(SimpleError::new(format!("unexpected day: {day}"))))
     }
 }
---
+++ src/y2017/day4.rs
@@ -0,0 +1,45 @@
+//! Day 4: High-Entropy Passphrases
+//! https://adventofcode.com/2017/day/4
+
+use std::collections::HashSet;
+use std::error::Error;
+use crate::SimpleError;
+
+fn solve_part_1(input: &str) -> Result<usize, SimpleError> {
+    let valid_count = input.lines().filter(|line| {
+        let mut words = HashSet::new();
+        for word in line.split(' ') {
+            if !words.insert(word) {
+                return false;
+            }
+        }
+        true
+    })
+        .count();
+
+    Ok(valid_count)
+}
+
+fn solve_part_2(input: &str) -> Result<usize, SimpleError> {
+    let valid_count = input.lines().filter(|line| {
+        let mut words = HashSet::new();
+        for word in line.split(' ') {
+            let mut word_chars: Vec<_> = word.chars().collect();
+            word_chars.sort();
+            if !words.insert(word_chars) {
+                return false;
+            }
+        }
+        true
+    })
+        .count();
+
+    Ok(valid_count)
+}
+
+pub fn solve(input: &str) -> Result<(usize, usize), Box<dyn Error>> {
+    let solution1 = solve_part_1(input)?;
+    let solution2 = solve_part_2(input)?;
+
+    Ok((solution1, solution2))
+}
