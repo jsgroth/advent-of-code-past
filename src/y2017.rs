2739b06810fb1c2a8298695190f8377cf7de4ff7
jsgroth <1137683+jsgroth@users.noreply.github.com>
2022-12-30 20:58:45 -0600

    2017 day 2

--- src/y2017.rs
+++ src/y2017.rs
@@ -1,4 +1,5 @@
 mod day1;
+mod day2;
 
 use std::error::Error;
 use crate::SimpleError;
@@ -6,6 +7,7 @@ use crate::SimpleError;
 pub fn run_day(day: usize) -> Result<(), Box<dyn Error>> {
     match day {
         1 => crate::run_solution(day1::solve),
+        2 => crate::run_solution(day2::solve),
         _ => Err(Box::new(SimpleError::new(format!("unexpected day: {day}"))))
     }
 }
---
+++ src/y2017/day2.rs
@@ -0,0 +1,58 @@
+//! Day 2: Corruption Checksum
+//! https://adventofcode.com/2017/day/2
+
+use std::cmp;
+use std::error::Error;
+use std::num::ParseIntError;
+use crate::SimpleError;
+
+fn solve_part_1(input: &str) -> Result<u32, SimpleError> {
+    let spreadsheet = parse_input(input)?;
+
+    let sum = spreadsheet.iter().map(|row| {
+        let (min, max) = row.iter().fold((u32::MAX, u32::MIN), |(min, max), &value| {
+            (cmp::min(min, value), cmp::max(max, value))
+        });
+        max - min
+    })
+        .sum();
+
+    Ok(sum)
+}
+
+fn solve_part_2(input: &str) -> Result<u32, SimpleError> {
+    let spreadsheet = parse_input(input)?;
+
+    let sum = spreadsheet.iter().map(|row| {
+        for (i, &a) in row.iter().enumerate() {
+            for &b in &row[i + 1..] {
+                if a % b == 0 {
+                    return Ok(a / b);
+                } else if b % a == 0 {
+                    return Ok(b / a);
+                }
+            }
+        }
+        Err(SimpleError::new(format!("row has no evenly divisible pair: {row:?}")))
+    })
+        .collect::<Result<Vec<_>, _>>()?
+        .into_iter().sum();
+
+    Ok(sum)
+}
+
+fn parse_input(input: &str) -> Result<Vec<Vec<u32>>, ParseIntError> {
+    input.lines().map(|line| {
+        line.split('\t')
+            .map(|word| word.parse::<u32>())
+            .collect()
+    })
+        .collect()
+}
+
+pub fn solve(input: &str) -> Result<(u32, u32), Box<dyn Error>> {
+    let solution1 = solve_part_1(input)?;
+    let solution2 = solve_part_2(input)?;
+
+    Ok((solution1, solution2))
+}
