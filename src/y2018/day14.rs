76ee63f86f2c857d153b595a8679455e009b8d39
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-01-02 23:37:30 -0600

    way faster, don't need a linked list for this

--- src/y2018/day14.rs
+++ src/y2018/day14.rs
@@ -1,110 +1,34 @@
 //! Day 14: Chocolate Charts
 //! https://adventofcode.com/2018/day/14
 
-use std::cell::RefCell;
 use std::error::Error;
-use std::rc::{Rc, Weak};
 use crate::SimpleError;
-
-#[derive(Debug)]
-struct ListNode {
-    val: u32,
-    next: Weak<RefCell<ListNode>>,
-}
-
-impl ListNode {
-    fn unwrap_next(&self) -> Rc<RefCell<ListNode>> {
-        self.next.upgrade().expect("every node should have a valid next pointer")
-    }
-}
-
-#[derive(Debug)]
-struct CircularList {
-    nodes: Vec<Rc<RefCell<ListNode>>>,
-}
-
-impl CircularList {
-    fn new(initial_value: u32) -> Self {
-        let head = Rc::new_cyclic(|me| {
-            RefCell::new(ListNode {
-                val: initial_value,
-                next: Weak::clone(me),
-            })
-        });
-        let nodes = vec![head];
-
-        Self { nodes }
-    }
-
-    fn head(&self) -> Rc<RefCell<ListNode>> {
-        Rc::clone(&self.nodes[0])
-    }
-
-    fn tail(&self) -> Rc<RefCell<ListNode>> {
-        Rc::clone(self.nodes.last().unwrap())
-    }
-
-    fn len(&self) -> usize {
-        self.nodes.len()
-    }
-
-    fn push(&mut self, value: u32) {
-        let new_node = Rc::new(RefCell::new(ListNode {
-            val: value,
-            next: Rc::downgrade(&self.nodes[0]),
-        }));
-
-        let tail = self.nodes.last().unwrap();
-        tail.borrow_mut().next = Rc::downgrade(&new_node);
-        self.nodes.push(new_node);
-    }
-}
 
 const INITIAL_STATE: [u32; 2] = [3, 7];
 
 fn solve_part_1(input: &str) -> Result<String, SimpleError> {
     let num_recipes: usize = crate::read_single_line(input)?.parse()?;
 
-    let mut circular_list = CircularList::new(INITIAL_STATE[0]);
-    circular_list.push(INITIAL_STATE[1]);
+    let mut list = Vec::from(INITIAL_STATE);
 
-    let mut elf0_node = circular_list.head();
-    let mut elf1_node = circular_list.tail();
-    while circular_list.len() < num_recipes + 10 {
-        let new_recipe = elf0_node.borrow().val + elf1_node.borrow().val;
+    let mut elf0_index = 0;
+    let mut elf1_index = 1;
+    while list.len() < num_recipes + 10 {
+        let new_recipe = list[elf0_index] + list[elf1_index];
         if new_recipe > 9 {
-            circular_list.push(1);
-            circular_list.push(new_recipe % 10);
+            list.push(1);
+            list.push(new_recipe % 10);
         } else {
-            circular_list.push(new_recipe);
+            list.push(new_recipe);
         }
 
-        let elf0_val = elf0_node.borrow().val;
-        for _ in 0..=elf0_val {
-            let next = elf0_node.borrow().unwrap_next();
-            elf0_node = next;
-        }
-
-        let elf1_val = elf1_node.borrow().val;
-        for _ in 0..=elf1_val {
-            let next = elf1_node.borrow().unwrap_next();
-            elf1_node = next;
-        }
+        elf0_index = (elf0_index + list[elf0_index] as usize + 1) % list.len();
+        elf1_index = (elf1_index + list[elf1_index] as usize + 1) % list.len();
     }
 
-    let mut current_node = circular_list.head();
-    for _ in 0..num_recipes {
-        let next = current_node.borrow().unwrap_next();
-        current_node = next;
-    }
-
-    let mut result = String::new();
-    for _ in 0..10 {
-        result.push(char::from_digit(current_node.borrow().val, 10).unwrap());
-
-        let next = current_node.borrow().unwrap_next();
-        current_node = next;
-    }
+    let result = list[num_recipes..num_recipes + 10].iter()
+        .map(|&digit| char::from_digit(digit, 10).unwrap())
+        .collect();
 
     Ok(result)
 }
@@ -114,14 +38,13 @@ fn solve_part_2(input: &str) -> Result<usize, SimpleError> {
         .map(|c| c.to_digit(10).ok_or(SimpleError::new(format!("not a digit: {c}"))))
         .collect::<Result<_, _>>()?;
 
-    let mut circular_list = CircularList::new(INITIAL_STATE[0]);
-    circular_list.push(INITIAL_STATE[1]);
+    let mut list = Vec::from(INITIAL_STATE);
 
-    let mut elf0_node = circular_list.head();
-    let mut elf1_node = circular_list.tail();
+    let mut elf0_index = 0;
+    let mut elf1_index = 1;
     let mut target_index = 0;
     loop {
-        let new_recipe = elf0_node.borrow().val + elf1_node.borrow().val;
+        let new_recipe = list[elf0_index] + list[elf1_index];
         let new_values = if new_recipe > 9 {
             vec![1, new_recipe % 10]
         } else {
@@ -140,25 +63,16 @@ fn solve_part_2(input: &str) -> Result<usize, SimpleError> {
             }
 
             if target_index == target_sequence.len() {
-                return Ok(circular_list.len() + (i + 1) - target_sequence.len());
+                return Ok(list.len() + (i + 1) - target_sequence.len());
             }
         }
 
         for &new_value in &new_values {
-            circular_list.push(new_value);
+            list.push(new_value);
         }
 
-        let elf0_val = elf0_node.borrow().val;
-        for _ in 0..=elf0_val {
-            let next = elf0_node.borrow().unwrap_next();
-            elf0_node = next;
-        }
-
-        let elf1_val = elf1_node.borrow().val;
-        for _ in 0..=elf1_val {
-            let next = elf1_node.borrow().unwrap_next();
-            elf1_node = next;
-        }
+        elf0_index = (elf0_index + list[elf0_index] as usize + 1) % list.len();
+        elf1_index = (elf1_index + list[elf1_index] as usize + 1) % list.len();
     }
 }
 
