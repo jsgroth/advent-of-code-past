e76eb4634b5722df6265535d2dff5e0c0a145c20
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-01-08 20:26:58 -0600

    2019 day 8

---
+++ src/y2019/day8.rs
@@ -0,0 +1,69 @@
+//! Day 8: Space Image Format
+//! https://adventofcode.com/2019/day/8
+
+use std::error::Error;
+use crate::SimpleError;
+
+fn solve_part_1(input: &str) -> Result<usize, SimpleError> {
+    let line = crate::read_single_line(input)?;
+    let chars: Vec<_> = line.chars().collect();
+
+    let mut fewest_zero_digits = usize::MAX;
+    let mut fewest_zero_digits_layer = Vec::new();
+    for layer in chars.chunks(25 * 6) {
+        let zero_digits = layer.iter().copied()
+            .filter(|&c| c == '0')
+            .count();
+
+        if zero_digits < fewest_zero_digits {
+            fewest_zero_digits = zero_digits;
+            fewest_zero_digits_layer = Vec::from(layer);
+        }
+    }
+
+    let ones = fewest_zero_digits_layer.iter().copied().filter(|&c| c == '1').count();
+    let twos = fewest_zero_digits_layer.iter().copied().filter(|&c| c == '2').count();
+
+    Ok(ones * twos)
+}
+
+fn solve_part_2(input: &str) -> Result<String, SimpleError> {
+    let line = crate::read_single_line(input)?;
+    let chars: Vec<_> = line.chars().collect();
+
+    let mut image = vec![vec![false; 25]; 6];
+    for row in 0..6 {
+        for col in 0..25 {
+            let c = chars.iter().copied()
+                .skip(25 * row + col)
+                .step_by(25 * 6)
+                .find(|&c| c != '2')
+                .ok_or(SimpleError::new(format!("no visible pixel at row={row}, col={col}")))?;
+            image[row][col] = c == '1';
+        }
+    }
+
+    let mut image_str = String::new();
+    for (i, row) in image.iter().enumerate() {
+        if i > 0 {
+            image_str.push('\n');
+        }
+
+        for &b in row {
+            if b {
+                image_str.push('█');
+            } else {
+                image_str.push(' ');
+            }
+        }
+    }
+
+    Ok(image_str)
+}
+
+pub fn solve(input: &str) -> Result<(usize, String), Box<dyn Error>> {
+    let solution1 = solve_part_1(input)?;
+    let solution2 = solve_part_2(input)?;
+
+    Ok((solution1, solution2))
+}
