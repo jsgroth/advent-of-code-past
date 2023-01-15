dc10376ff6a09d3ad77b0344ecba9c8766c75631
jsgroth <1137683+jsgroth@users.noreply.github.com>
2023-01-15 13:46:43 -0600

    2020 day 6

---
+++ src/y2020/day6.rs
@@ -0,0 +1,44 @@
+//! Day 6: Custom Customs
+//! https://adventofcode.com/2020/day/6
+
+use std::collections::HashSet;
+use std::error::Error;
+use crate::SimpleError;
+
+fn solve_part_1(input: &str) -> Result<u32, SimpleError> {
+    let lines: Vec<_> = input.lines().collect();
+
+    let total_answered = lines.split(|s| s.is_empty())
+        .map(|group| {
+            group.iter()
+                .flat_map(|line| line.chars())
+                .collect::<HashSet<_>>()
+                .len() as u32
+        })
+        .sum();
+
+    Ok(total_answered)
+}
+
+fn solve_part_2(input: &str) -> Result<u32, SimpleError> {
+    let lines: Vec<_> = input.lines().collect();
+
+    let total_all_answered = lines.split(|s| s.is_empty())
+        .map(|group| {
+            group.iter()
+                .map(|line| line.chars().collect::<HashSet<_>>())
+                .reduce(|a, b| a.intersection(&b).copied().collect::<HashSet<_>>())
+                .unwrap()
+                .len() as u32
+        })
+        .sum();
+
+    Ok(total_all_answered)
+}
+
+pub fn solve(input: &str) -> Result<(u32, u32), Box<dyn Error>> {
+    let solution1 = solve_part_1(input)?;
+    let solution2 = solve_part_2(input)?;
+
+    Ok((solution1, solution2))
+}
